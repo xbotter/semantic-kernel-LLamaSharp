ce7fa0294bd2ea938fc2cfee64b4f216520142b6
xbotter <xbotter@users.noreply.github.com>
2023-06-25 09:24:21 +0800

    Remove system prompt and use lead words instead (#8)
    * Remove system prompt and use lead words instead
    
    * remove unused code

---
+++ src/Connectors.AI.LLamaSharp/ChatCompletion/HistoryTransform.cs
@@ -0,0 +1,18 @@
+﻿using static LLama.LLamaTransforms;
+
+namespace Connectors.AI.LLamaSharp.ChatCompletion
+{
+    /// <summary>
+    /// Default HistoryTransform Patch
+    /// </summary>
+    public class HistoryTransform : DefaultHistoryTransform
+    {
+        /// <inheritdoc/>
+        public override string HistoryToText(LLama.Common.ChatHistory history)
+        {
+            var prompt = base.HistoryToText(history);
+            return prompt + "\nAssistant:";
+
+        }
+    }
+}
